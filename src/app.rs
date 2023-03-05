ebf86177f135af76d51024ac56762602df773294
Bogdan Olar <olar.bogdan.dev@gmail.com>
2023-03-05 10:13:35 +0200

    Update info text with correct repository link

--- src/app.rs
+++ src/app.rs
@@ -207,7 +207,7 @@ impl MinesweepRsApp {
                     ui.separator();
                     ui.add(Label::new("Copyright (c) 2022 Bogdan Olar"));
                     ui.separator();
-                    ui.hyperlink("https://github.com/BogdanOlar/minesweep-rs");
+                    ui.hyperlink("https://github.com/BogdanOlar/egui-minesweep-rs");
                     ui.separator();
                     ui.add(Label::new("Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the \"Software\"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:"));
                     ui.add(Label::new("The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software."));
